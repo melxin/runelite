67606ee162fb14c4f6fc85ec398dff3efef6b9b1
melxin <melxin@tutamail.com>
2025-02-21 21:46:10 +0100

    mixins: fix player & npc spawns

---
+++ runelite-mixins/src/main/java/net/runelite/mixins/RSIndexedObjectNodeMixin.java
@@ -0,0 +1,28 @@
+package net.runelite.mixins;
+
+import net.runelite.api.Player;
+import net.runelite.api.events.PlayerDespawned;
+import net.runelite.api.mixins.Inject;
+import net.runelite.api.mixins.MethodHook;
+import net.runelite.api.mixins.Mixin;
+import net.runelite.api.mixins.Shadow;
+import net.runelite.rs.api.RSClient;
+import net.runelite.rs.api.RSIndexedObjectNode;
+import net.runelite.rs.api.RSPlayer;
+
+@Mixin(RSIndexedObjectNode.class)
+public abstract class RSIndexedObjectNodeMixin implements RSIndexedObjectNode
+{
+	@Shadow("client")
+	private static RSClient client;
+
+	@MethodHook("detach")
+	@Inject
+	public void onDetach()
+	{
+		if (this instanceof RSPlayer)
+		{
+			client.getCallbacks().postDeferred(new PlayerDespawned((Player) this));
+		}
+	}
+}
