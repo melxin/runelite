dfa111159a4f815188716fe80208a21dea543cae
melxin <melxin@tutamail.com>
2025-04-10 09:05:14 +0200

    mixins: (TileObject) fix id
    9e07023e1bd5f7b5808eed1474df7ac6fe5af706 06bef897aab950678d8ab337fa1dc1533c344df7 b553ab82f515fa3dc927ce637bfd4d105af86b3a

--- runelite-mixins/src/main/java/net/runelite/mixins/RSTileObjectMixin.java
+++ runelite-mixins/src/main/java/net/runelite/mixins/RSTileObjectMixin.java
@@ -36,7 +36,7 @@ public abstract class RSTileObjectMixin implements TileObject
 	public int getId()
 	{
 		long hash = getHash();
-		return (int) (hash >>> 18 & 4294967295L);
+		return (int) (hash >>> 20 & 4294967295L);
 	}
 	
 	@Override
