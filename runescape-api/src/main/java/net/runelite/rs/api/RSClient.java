c22c662ae809b7925c7b31f6b7c5ae1ce8de856d
melxin <melxin@tutamail.com>
2025-06-05 01:28:19 +0200

    rs-client: add getEnvironment
    4bd30ff31b37a242d92ece661c495524acdb27a6 ee9e92f5838ae03d1d7798bcc057e525bddca383

--- runescape-api/src/main/java/net/runelite/rs/api/RSClient.java
+++ runescape-api/src/main/java/net/runelite/rs/api/RSClient.java
@@ -40,6 +40,10 @@ import net.runelite.mapping.Import;
 
 public interface RSClient extends RSGameEngine, Client
 {
+	@Import("gameBuild")
+	@Override
+	int getEnvironment();
+
 	@Import("cameraX")
 	@Override
 	int getCameraX();
