49cf569dc36f53f238e1398cc495491f2204a7d8
melxin <melxin@tutamail.com>
2025-08-06 16:31:36 +0200

    project: Bump RS version

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -29,7 +29,7 @@ object ProjectVersions {
 
     const val openosrsVersion = "4.31.2"
 
-    const val rsversion = 231
+    const val rsversion = 232
     const val cacheversion = 165
 
     const val lombokVersion = "1.18.30"
